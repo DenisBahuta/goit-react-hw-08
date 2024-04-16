8a4ee6124a19dcf2769d39d48b49ba3d5003e2fa
DenisBahuta <denisboguto@gmail.com>
2024-04-16 16:23:30 +0200

    update Contacts

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,11 +1,17 @@
 import { Helmet } from "react-helmet-async";
 import ContactForm from "../components/ContactForm/ContactForm";
+import SearchBox from "../components/SearchBox/SearchBox";
+import ContactList from "../components/ContactList/ContactList";
 import { fetchContacts } from "../redux/contacts/operations";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
+import Loader from "../components/Loader/Loader";
+import { selectError, selectLoading } from "../redux/contacts/selectors";
 
 const Contacts = () => {
   const dispatch = useDispatch();
+  const isLoading = useSelector(selectLoading);
+  const isError = useSelector(selectError);
 
   useEffect(() => {
     dispatch(fetchContacts());
@@ -17,6 +23,16 @@ const Contacts = () => {
         <title>Contacts</title>
       </Helmet>
       <ContactForm />
+      <SearchBox />
+
+      {isLoading && !isError && <Loader />}
+      {isError && (
+        <b>
+          There is a problem with the connection to the server, please try again
+          later
+        </b>
+      )}
+      <ContactList />
     </div>
   );
 };
